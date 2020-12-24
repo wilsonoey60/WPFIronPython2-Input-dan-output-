87dc65798d4fa26558de5ee1f212d1f298b0a4c0
Wilson Oey <oey_wilson@hotmail.com>
2020-12-24 15:04:59 +0800

    Add files via upload

---
+++ Input dan output/Input dan output/Application.py
@@ -0,0 +1,8 @@
+﻿import wpf
+
+from System.Windows import Application
+from Window1 import Window1
+
+window = Window1()
+app = Application()
+app.Run(window)
---
+++ Input dan output/Input dan output/Window1.py
@@ -0,0 +1,15 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+class Window1(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'Window1.xaml')
+        
+    def button1_Click(self, sender, e):
+        a = self.inputan.Text
+        self.cetak.Content = a
+        pass
+        
+if __name__ == '__main__':
+    Application().Run(MyWindow())
